93adffbff696e31eaf82eb0b3122f53a055d49bf
Henry Box <14365005+henrybox@users.noreply.github.com>
2025-05-17 15:06:48 -0300

    WIP: refactoring

--- tests/test-bundler/index.js
+++ tests/test-bundler/index.js
@@ -1,25 +1,47 @@
 const Workspace = require('beyond/workspace');
 const { join } = require('path');
+require('colors');
 
 const path = join(__dirname, 'files');
 const workspace = new Workspace(path);
 
 (async () => {
 	await workspace.packages.ready;
-	console.log('Packages found:');
-
-	for (const [id, pkg] of workspace.packages) {
+	for (const pkg of workspace.packages.values()) {
 		await pkg.ready;
-		console.log('•', id, pkg.name);
-
 		await pkg.modules.ready;
-		console.log(`  (${pkg.modules.size} Modules found)`);
+
+		// Show package information
+		const { errors, warnings } = pkg.bundlers;
+		console.log('Package:'.green.bold, pkg.vname.green);
+		console.log(`  • Registered bundlers: ${[...pkg.bundlers.keys()].join(', ')}`);
+		errors.length && console.log(`  • Bundlers errors: ${[...pkg.bundlers.errors].join(', ')}`);
+		warnings.length && console.log(`  • Bundlers warnings: ${[...pkg.bundlers.warnings].join(', ')}`);
+
+		// Show modules information
+		console.log('\nShow modules information\n'.green.bold);
 
 		for (const module of pkg.modules.values()) {
 			await module.ready;
-			console.log(`  • Module: ${module.id}`);
+			console.log('Module:'.green.bold, module.subpath?.green);
+
+			const { errors, warnings } = module;
+			errors ?? console.log(`  • Errors found: [...${module.errors}].join(', ')}`);
+			warnings ?? console.log(`  • Warnings found: [...${module.warnings}].join(', ')}`);
 
 			await module.conditionals.ready;
+			console.log('  • Conditionals:', [...module.conditionals.keys()].join(', '));
+
+			const conditional = module.conditionals.get('default');
+			if (conditional) {
+				await conditional.ready;
+				console.log('  • Bundler:', conditional.bundler?.vname);
+				console.log('  • Bundler specs:', conditional.bundler?.specs);
+				console.log('  • Bundler settings:', conditional.bundler?.settings);
+				console.log('  • Bundler processors:', conditional.bundler?.processors);
+			} else {
+				console.log('  • No default bundler found');
+			}
 		}
 	}
 
